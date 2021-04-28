8a83926b321eb002c6bf4bf25ed3182888f88621
Taras Sorochynskyi <sorochynskyi_taras@ukr.net>
2021-04-28 10:09:10 +0300

    test

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
-  const cats = backyard.flat().filter(elem => elem === '^^');
+  const cats = backyard.flat().filter(el => el === '^^');
   return cats.length;
 };
